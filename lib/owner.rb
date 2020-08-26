a90705ba45f0c0494a1a6522bc88fe882811f52e
daniel campo <campo_daniel94@yahoo.com>
2020-08-26 22:56:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
 class Owner
-  # code goes here
+   
+   @@all = []
+  
+   attr_reader :name, :species
+   
+   def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+   end
+    
+    def say_species
+       "I am a human."
+    end
+    
+    def self.all
+      @@all
+    end
+    
+    def self.count
+      Owner.all.count
+    end
+    
+    def self.reset_all
+      all.clear
+    end
+    
+    def cats
+       Cats.all.select { |cats| cats.owner }
+         
+  end
+  
+  
+  
 end
